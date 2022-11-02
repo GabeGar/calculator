9ac9fd82fa370fbc49ed9f4ae3628c131437df88
Gabegar <Gagarcia190@gmail.com>
2022-11-02 14:51:57 -0400

    creates: operators object & calculate function
    1. Operators object dictionary, contains all the basic math operators,
    along with their relevant operational function, in key-value pairs.
    
    2. The calculate function will handle button click event listeners,
    taking the symbol from said button and using it to access an operator,
    within the operators object.

---
+++ script.js
@@ -0,0 +1,27 @@
+// Dictionary object of operators, which will be used as call back handler funcs.
+const operators = {
+    "+": function add(a, b) {
+        return a + b;
+    },
+
+    "-": function subtract(a, b) {
+        return a - b;
+    },
+
+    "&times;": function multiply(a, b) {
+        return a * b;
+    },
+
+    "&divide;": function divide(a, b) {
+        return a / b;
+    },
+};
+
+// Calculate function that takes the symbol based on the button press (event listeners to be added),
+// passes it in, along with two numbers, as parameters.
+
+function calculate(symbol, a, b) {
+    return operators[symbol](a, b);
+}
+
+// console.log(calculate("&times;", 2, 10));
